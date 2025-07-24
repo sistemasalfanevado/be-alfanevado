52c5630f474589c166f5afc4478d48c4e5ef7692
Victor Cahui <vfcahui@gmail.com>
2025-07-23 23:39:59 -0500

    Add DTO detection and whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ❗ Elimina campos que no están en el DTO
+    }),
+  );
 
   // Habilitar CORS
   app.enableCors({
