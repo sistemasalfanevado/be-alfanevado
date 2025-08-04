faf3d6ad111aff0434fdf190ffd95984dbb60234
Victor Cahui <vfcahui@gmail.com>
2025-08-04 01:07:49 -0500

    Fix zentra bank account module import

--- src/modules/zentra/zentra-master/zentra-party-bank-account/dto/create-zentra-party-bank-account.dto.ts
+++ src/modules/zentra/zentra-master/zentra-party-bank-account/dto/create-zentra-party-bank-account.dto.ts
@@ -2,17 +2,14 @@ import { IsString, IsNotEmpty, MaxLength, IsOptional } from 'class-validator';
 
 export class CreateZentraPartyBankAccountDto {
   @IsString()
-  @IsNotEmpty()
   @MaxLength(30, { message: 'Account must not exceed 30 characters' })
   account: string;
 
   @IsString()
-  @IsNotEmpty()
   @MaxLength(50, { message: 'CCI must not exceed 50 characters' })
   cci: string;
 
   @IsString()
-  @IsNotEmpty()
   @MaxLength(100, { message: 'Description must not exceed 100 characters' })
   description: string;
 
