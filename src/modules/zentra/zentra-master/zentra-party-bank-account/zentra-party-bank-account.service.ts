535d8133e3f0f95ed60e8cdb3327e27ba5e95548
Victor Cahui <vfcahui@gmail.com>
2025-11-18 14:21:07 -0500

    Update bank account id

--- src/modules/zentra/zentra-master/zentra-party-bank-account/zentra-party-bank-account.service.ts
+++ src/modules/zentra/zentra-master/zentra-party-bank-account/zentra-party-bank-account.service.ts
@@ -2,14 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../../prisma/prisma.service';
 import { CreateZentraPartyBankAccountDto } from './dto/create-zentra-party-bank-account.dto';
 import { UpdateZentraPartyBankAccountDto } from './dto/update-zentra-party-bank-account.dto';
+import { BANK_ACCOUNT_HIERARCHY } from 'src/shared/constants/app.constants';
 
 @Injectable()
 export class ZentraPartyBankAccountService {
   constructor(private prisma: PrismaService) { }
 
-  async create(createZentraPartyBankAccountDto: CreateZentraPartyBankAccountDto) {
+  async create(dto: CreateZentraPartyBankAccountDto) {
+
+    if (dto.hierarchyId === BANK_ACCOUNT_HIERARCHY.PRINCIPAL) {
+      await this.prisma.zentraPartyBankAccount.updateMany({
+        where: {
+          partyId: dto.partyId,
+          hierarchyId: BANK_ACCOUNT_HIERARCHY.PRINCIPAL,
+        },
+        data: {
+          hierarchyId: BANK_ACCOUNT_HIERARCHY.ADICIONAL,
+        },
+      });
+    }
+
     await this.prisma.zentraPartyBankAccount.create({
-      data: createZentraPartyBankAccountDto,
+      data: dto,
     });
 
     return { message: "Cuenta bancaria creada correctamente" };
@@ -101,10 +115,39 @@ export class ZentraPartyBankAccountService {
     });
   }
 
-  async update(id: string, updateZentraPartyBankAccountDto: UpdateZentraPartyBankAccountDto) {
+  async update(
+    id: string,
+    dto: UpdateZentraPartyBankAccountDto
+  ) {
+
+    // 1. Obtener la cuenta actual para saber el partyId
+    const current = await this.prisma.zentraPartyBankAccount.findUnique({
+      where: { id },
+    });
+
+    if (!current) {
+      throw new Error("Cuenta bancaria no encontrada");
+    }
+
+    // 2. Si esta actualización marca la cuenta como PRINCIPAL,
+    // entonces bajar a ADICIONAL las otras cuentas del mismo party.
+    if (dto.hierarchyId === BANK_ACCOUNT_HIERARCHY.PRINCIPAL) {
+      await this.prisma.zentraPartyBankAccount.updateMany({
+        where: {
+          partyId: current.partyId,
+          id: { not: id }, // excluir la que se está actualizando
+          hierarchyId: BANK_ACCOUNT_HIERARCHY.PRINCIPAL,
+        },
+        data: {
+          hierarchyId: BANK_ACCOUNT_HIERARCHY.ADICIONAL,
+        },
+      });
+    }
+
+    // 3. Actualizar la cuenta normalmente
     return this.prisma.zentraPartyBankAccount.update({
       where: { id },
-      data: updateZentraPartyBankAccountDto,
+      data: dto,
     });
   }
 
